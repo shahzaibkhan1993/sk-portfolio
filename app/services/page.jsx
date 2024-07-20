d8f844c45d9f947bebca0179774b2e2db1c08924
Shahzeb <shahzaib201411@gmail.com>
2024-07-20 23:38:26 +0500

    added

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -95,9 +95,9 @@ const Services = () => {
                 {/* title */}
                 <h2 className=" text-center text-wrap  text-[28px] font-bold leading-snug text-white group-hover:text-accent transition-all duration-500">{service.title}</h2>
                 </div>
-                <div className="flex justify-center items-center">
+                <div className="flex justify-center items-center text-wrap flex-wrap sm:text-wrap">
                 {/* description */}
-                <p className=" text-white/60 text-justify text-wrap break-words">{service.description}</p>
+                <p className=" text-white/60 text-justify  text-wrap break-words">{service.description}</p>
                 </div>
                 
                 
@@ -119,97 +119,3 @@ const Services = () => {
 }
 
 export default Services;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// "use client";
-// import {BsArrowDownRight}from 'react-icons/bs';
-// import Link from 'next/link';
-// import { motion } from 'framer-motion';
-
-// const services=[
-//   {
-//     num:'01',
-//     title:'Web Development',
-//     description:"Passionate web developer with a strong foundation in front-end & back-end technologies. Skilled in building responsive websites as well as web applications using variety of programming languages like Next.js, React, Angular, PHP, ASP.NET MVC, REST APIs, HTML, CSS / Tailwind CSS, JavaScript. Proven ability to design, develop, and maintain complex web applications.",
-//     href:"/work"
-//   },
-//   {
-//     num:'02',
-//     title:'Mobile Development',
-//     description:"Mobile app developer adept at building native and cross-platform applications. Proficient in languages like Java/Kotlin (Android) along with frameworks like React Native or Flutter for cross-platform development. Experience in crafting user-friendly interfaces, integrating APIs, and optimizing app performance for a seamless mobile experience.",
-//     href:"/work"
-//   },
-//   {
-//     num:'03',
-//     title:'UI/UX Design',
-//     description:"Creative UI/UX designer with a proficiency in Figma and design principles. Skilled in crafting user-centered interfaces that are both visually appealing and intuitive. Experience in designing and prototyping interactive experiences using Figma for web and mobile applications.",
-//     href:"/work"
-//   },
-  
-//   {
-//     num:'04',
-//     title:'Data Anlaysis & Visualization',
-//     description:"Data enthusiast with expertise in wrangling and analyzing data using Python and its libraries (Pandas, NumPy, Matplotlib, Seaborn). Skilled at creating compelling data visualizations with Tableau and Power BI to communicate insights effectively. Experience in cleaning, manipulating, and transforming data to uncover patterns and trends.",
-//     href:"/work"
-//   },
-//   {
-//     num:'05',
-//     title:'Generative AI Developer',
-//     description:"AI-powered developer passionate about integrating generative AI into modern software applications. Leverages Python frameworks (Flask, Django, Streamlit, FastAPI) to build engaging generative AI applications. Possesses a strong understanding of AI concepts and conversational interfaces. Adept at designing and deploying scalable AI models in the cloud using platforms such as Hugging Face, TensorFlow, NLP, LLM, and ML. Develops AI-powered solutions using cutting-edge libraries like Face++, Face Recognition, Voice Recognition, ResNet, GAN, and librosa to automate tasks and empower intelligent decision-making.",
-//     href:"/work"
-//   }
- 
-// ];
-
-
-// const Services = () => {
-//   return (
-//     <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
-//       <div className="container mx-auto scroll-m-10">
-//       <motion.div
-//       initial={{opacity:0}}
-//       animate={{opacity:1,transition:{delay:2.4,duration:0.4,ease:"easeIn"}}}
-//       className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
-//       >
-//         {services.map((service,index)=>{
-//           return <div key={index} className='flex-1 flex flex-col justify-center gap-6 group'>
-//             {/* top */}
-//             <div className="w-full flex justify-between items-center">
-//               <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">{service.num}</div>
-//               <Link href={service.href} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45">
-//               <BsArrowDownRight className="text-primary text-3xl" />
-//               </Link>
-//             </div>
-//             {/* title */}
-//             <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">{service.title}</h2>
-//             {/* description */}
-//             <p className="text-white/60 text-justify">{service.description}</p>
-//             {/* border */}
-//             <div className="border-b border-white/20 w-full"></div>
-//           </div>
-//         })}
-//       </motion.div>
-//       </div>
-//     </section>
-//   )
-// }
-
-// export default Services
